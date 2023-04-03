4fc1cce2657c63a7220e6756cc44c87a7c7a5316
Roxanne Liu <roxanneliu819@gmail.com>
2023-04-03 23:36:15 +1000

    second commit

---
+++ js/project0.js
@@ -0,0 +1,65 @@
+$(document).ready(function () { 
+
+    $('#top-left').on('click', function () {
+        
+    });
+
+
+    $('#top-center').on('click', function () {
+        
+    });
+
+
+    $('#top-right').on('click', function () {
+        
+    });
+
+
+    $('#middle-left').on('click', function () {
+        
+    });
+
+
+    $('#middle-center').on('click', function () {
+        
+    });
+
+
+    $('#middle-right').on('click', function () {
+        
+    });
+
+
+
+    $('#bottom-left').on('click', function () {
+        
+    });
+
+
+
+    $('#bottom-left').on('click', function () {
+        
+    });
+
+
+    $('#bottom-center').on('click', function () {
+        
+    });
+
+
+
+    $('#bottom-right').on('click', function () {
+        
+    });
+
+
+
+
+
+
+
+
+
+
+
+});
